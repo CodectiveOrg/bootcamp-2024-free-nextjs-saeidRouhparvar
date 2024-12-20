3ec9744aeed227ea9eebf0fa220478992f648da0
saeid.rohparvar <saeid.rohparvar@gmail.com>
2024-12-20 17:09:16 +0330

    [REFACTOR] useTheme

--- src/app/hooks/useTheme.tsx
+++ src/app/hooks/useTheme.tsx
@@ -2,19 +2,27 @@
 import { useState, useEffect } from "react";
 
 export const useTheme = () => {
-  const [theme, setTheme] = useState(
-    () => localStorage.getItem("theme") || "light",
-  );
+  const [theme, setTheme] = useState("light"); 
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedTheme = localStorage.getItem("theme") || "light";
+      setTheme(storedTheme);
+    }
+  }, []);
 
   useEffect(() => {
     const root = document.documentElement;
     root.classList.remove("light-theme", "dark-theme");
     root.classList.add(`${theme}-theme`);
-    localStorage.setItem("theme", theme);
+    
+    if (typeof window !== "undefined") {
+      localStorage.setItem("theme", theme);
+    }
   }, [theme]);
 
   const toggleTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
 
   return { theme, toggleTheme };
