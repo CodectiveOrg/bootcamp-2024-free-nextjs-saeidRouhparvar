ecb7e965231cb38900172a71dfff5d7211f056ba
saeid.rohparvar <saeid.rohparvar@gmail.com>
2025-01-08 20:00:35 +0330

    create doctor card

---
+++ src/app/search/drCard/drCard.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import style from "./drCard.module.css";
+import { Button } from "@/components/uiKit/Button";
+import Avatar from "@/components/avatar/avatar";
+
+import MainBox from "@/components/mainBox/mainBox";
+import Tag from "@/components/tag/Tag";
+
+type DrCardProps = {
+  avatar: string;
+  drName: string;
+  drDesc: string;
+  address: string;
+  tag: string[];
+};
+
+const DrCard = ({ avatar, drName, drDesc, address, tag }: DrCardProps) => {
+  return (
+    <MainBox className={style.main}>
+      <div className={style.main_content}>
+        <div className={style.avatar}>
+          <Avatar src={avatar} />
+        </div>
+        <div className={style.data}>
+          <div className={style.dr_name}>{drName}</div>
+          <div className={style.desc}>{drDesc}</div>
+        </div>
+      </div>
+
+      <div className={style.address_tag}>
+        <div>{address}</div>
+        <div className={style.tags}>
+          {tag?.map((item: string) => <Tag>{item}</Tag>)}
+        </div>
+      </div>
+      <div className={style.btns}>
+        <Button>ویزیت آنلاین</Button>
+        <Button>نوبت اینترنتی</Button>
+      </div>
+    </MainBox>
+  );
+};
+
+export default DrCard;
