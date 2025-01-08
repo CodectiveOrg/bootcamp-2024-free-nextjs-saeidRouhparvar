a64b26cd52e45edd70c93351141613c97a21aba1
saeid.rohparvar <saeid.rohparvar@gmail.com>
2025-01-08 20:03:07 +0330

    create tag

--- src/components/tag/Tag.tsx
+++ src/components/tag/Tag.tsx
@@ -1,16 +1,22 @@
 import React, { HTMLAttributes, PropsWithChildren } from "react";
-import style from "./tag.module.css";
 import clsx from "clsx";
+
+import style from "./tag.module.css";
+import MingcuteCloseLine from "@/icons/MingcuteCloseLine";
 
 type TagProps = PropsWithChildren &
   HTMLAttributes<HTMLDivElement> & {
     className?: string;
+    onClick?: () => void;
   };
 
-const Tag = ({ children, className, ...rest }: TagProps) => {
+const Tag = ({ children, className, onClick, ...rest }: TagProps) => {
   return (
     <div className={clsx(style.tag, className)} {...rest}>
       {children}
+      {onClick && (
+        <MingcuteCloseLine onClick={onClick} className={style.close} />
+      )}
     </div>
   );
 };
